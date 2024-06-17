333fc2b3f10fa2c0d978e86b9f74d942a3f3990f
rajapedd <rajapeddanna.kadavakallu@gmail.com>
2024-06-17 22:07:28 +0530

    Second Commit Changes

--- Module1/src/Main.java
+++ Module1/src/Main.java
@@ -1,5 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        List<Integer> li= new ArrayList<>();
+        System.out.println(li.size());
     }
 }
